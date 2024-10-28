7c1d70e23de052997a08d5690bcf559620abb036
Devaraj <nsdevaraj@gmail.com>
2024-10-28 19:34:10 +0530

    remove duplicates

--- src/components/FiscalDatePicker.tsx
+++ src/components/FiscalDatePicker.tsx
@@ -150,6 +150,7 @@ const FiscalDatePicker: React.FC<FiscalDatePickerProps> = ({ fiscalStartMonth = 
         localMonths = [...localMonths, monthKey];
       }
     });
+    localMonths = [...new Set(localMonths)];
     setSelectedMonths([...localMonths])
 
   };
